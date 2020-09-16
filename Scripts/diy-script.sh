510a6fb5c773f5f60326ada8ea9072b6a2cb34fb
Hyy2001X <1804430051@qq.com>
2020-09-16 17:54:29 +0800

    Update diy-script.sh

--- Scripts/diy-script.sh
+++ Scripts/diy-script.sh
@@ -1,49 +1,45 @@
 #!/bin/bash
 # AutoBuild Script Module by Hyy2001
-# Actions
+# AutoBuild Actions
 
 Diy_Core() {
-#自定义作者、配置等信息
 Author=Hyy2001
-Github=https://github.com/Hyy2001X/Openwrt-AutoUpdate
+AutoUpdate_Github=https://github.com/Hyy2001X/Openwrt-AutoUpdate
 TARGET_BOARD=ramips
 TARGET_SUBTARGET=mt7621
 TARGET_PROFILE=d-team_newifi-d2
 }
 
-ExtraPackages_GIT() {
-[ -d ./package/lean/$1 ] && rm -rf ./package/lean/$1
-while [ ! -f $1/Makefile ]
+ExtraPackages() {
+[ -d ./package/lean/$2 ] && rm -rf ./package/lean/$2
+[ -d ./$2 ] && rm -rf ./$2
+while [ ! -f $2/Makefile ]
 do
-	git clone -b $3 $2/$1 $1
+	echo "Checking out $2 from $3 ..."
+	if [ $1 == git ];then
+		git clone -b $4 $3/$2 $2 > /dev/null 2>&1
+	else
+		svn checkout $3/$2 $2 > /dev/null 2>&1
+	fi
+	if [ ! -f $2/Makefile ];then
+		echo "Checkout failed,retry in 3s."
+		rm -rf $2 > /dev/null 2>&1
+		sleep 3
+	fi
 done
-mv $1 ./package/lean
-}
-
-ExtraPackages_SVN() {
-[ -d ./package/lean/$1 ] && rm -rf ./package/lean/$1
-while [ ! -f $1/Makefile ]
-do
-	echo "Checking out $1 from $2 ..."
-	svn checkout $2/$1 $1 > /dev/null 2>&1
-done
-echo "Package $1 detected!"
-mv $1 ./package/lean
+echo "Package $2 detected!"
+mv $2 ./package/lean
 }
 
 Diy-Part1() {
 sed -i "s/#src-git helloworld/src-git helloworld/g" feeds.conf.default
-#添加额外的软件包,使用方法: 
-#[git clone] ExtraPackages_GIT 软件包名称 仓库地址 分支
-#[svn checkout] ExtraPackages_SVN 软件包名称 仓库地址
-ExtraPackages_GIT luci-theme-argon https://github.com/jerrykuku 18.06
-ExtraPackages_SVN luci-app-openclash https://github.com/vernesong/OpenClash/trunk
-ExtraPackages_SVN luci-app-adguardhome https://github.com/Lienol/openwrt/trunk/package/diy
-ExtraPackages_SVN luci-app-smartdns https://github.com/project-openwrt/openwrt/trunk/package/ntlf9t
-ExtraPackages_SVN smartdns https://github.com/project-openwrt/openwrt/trunk/package/ntlf9t
-#添加 AutoUpdate 自动更新脚本到固件
+ExtraPackages git luci-theme-argon https://github.com/jerrykuku 18.06
+ExtraPackages svn luci-app-openclash https://github.com/vernesong/OpenClash/trunk
+ExtraPackages svn luci-app-adguardhome https://github.com/Lienol/openwrt/trunk/package/diy
+ExtraPackages svn luci-app-smartdns https://github.com/project-openwrt/openwrt/trunk/package/ntlf9t
+ExtraPackages svn smartdns https://github.com/project-openwrt/openwrt/trunk/package/ntlf9t
 [ -d ./Openwrt-AutoUpdate ] && rm -rf ./Openwrt-AutoUpdate
-git clone $Github
+git clone -b master $AutoUpdate_Github
 mv Openwrt-AutoUpdate/AutoUpdate.sh ./package/base-files/files/bin
 }
 
