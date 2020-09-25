092f75915bc4b4f3e4408b70cd91cdc6aec517f7
Hyy2001X <1804430051@qq.com>
2020-09-25 11:00:40 +0800

    Add files via upload

--- Scripts/diy-script.sh
+++ Scripts/diy-script.sh
@@ -7,16 +7,15 @@ Diy_Core() {
 Author=Hyy2001
 
 Default_File=./package/lean/default-settings/files/zzz-default-settings
-FIRMWARE_SUFFIX=squashfs-sysupgrade.bin
 Lede_Version=`egrep -o "R[0-9]+\.[0-9]+\.[0-9]+" $Default_File`
 Compile_Date=`date +'%Y/%m/%d'`
 Compile_Time=`date +'%Y-%m-%d %H:%M:%S'`
+TARGET_PROFILE=`grep '^CONFIG_TARGET.*DEVICE.*=y' .config | sed -r 's/.*DEVICE_(.*)=y/\1/'`
 }
 
 GET_TARGET_INFO() {
 TARGET_BOARD=`awk -F'[="]+' '/TARGET_BOARD/{print $2}' .config`
 TARGET_SUBTARGET=`awk -F'[="]+' '/TARGET_SUBTARGET/{print $2}' .config`
-TARGET_PROFILE=`grep '^CONFIG_TARGET.*DEVICE.*=y' .config | sed -r 's/.*DEVICE_(.*)=y/\1/'`
 }
 
 ExtraPackages() {
@@ -48,6 +47,10 @@ done
 }
 
 Diy-Part1() {
+mv $GITHUB_WORKSPACE/Customize/mac80211.sh openwrt/package/kernel/mac80211/files/lib/wifi/mac80211.sh
+mv $GITHUB_WORKSPACE/Customize/AutoUpdate.sh openwrt/package/base-files/files/bin/AutoUpdate.sh
+mv $GITHUB_WORKSPACE/Customize/system openwrt/package/base-files/files/etc/config/system
+#mv $GITHUB_WORKSPACE/Customize/mwan3 openwrt/package/feeds/packages/mwan3/files/etc/config/mwan3
 sed -i "s/#src-git helloworld/src-git helloworld/g" feeds.conf.default
 [ ! -d ./package/lean ] && mkdir ./package/lean
 ExtraPackages git luci-theme-argon https://github.com/jerrykuku 18.06
@@ -55,32 +58,31 @@ ExtraPackages svn luci-app-adguardhome https://github.com/Lienol/openwrt/trunk/p
 ExtraPackages svn luci-app-smartdns https://github.com/project-openwrt/openwrt/trunk/package/ntlf9t
 ExtraPackages svn smartdns https://github.com/project-openwrt/openwrt/trunk/package/ntlf9t
 ExtraPackages git OpenClash https://github.com/vernesong master
+ExtraPackages git luci-app-serverchan https://github.com/tty228 master
+ExtraPackages svn luci-app-socat https://github.com/xiaorouji/openwrt-package/trunk/lienol
 }
 
 Diy-Part2() {
-echo "[$(date "+%H:%M:%S")] Current Openwrt version: $Lede_Version-`date +%Y%m%d`"
-if [ ! $(grep -o "Compiled by $Author" $Default_File | wc -l) = "1" ];then
-	sed -i "s?$Lede_Version?$Lede_Version Compiled by $Author [$Compile_Date]?g" $Default_File
-fi
+echo "Author: $Author"
+echo "Current Openwrt version: $Lede_Version-`date +%Y%m%d`"
+echo "Current Device: $TARGET_PROFILE"
+sed -i "s?$Lede_Version?$Lede_Version Compiled by $Author [$Compile_Date]?g" $Default_File
+echo "$Author" > ./package/base-files/files/etc/openwrt_author
 echo "$Lede_Version-`date +%Y%m%d`" > ./package/base-files/files/etc/openwrt_date
-echo "[$(date "+%H:%M:%S")] Writing $Lede_Version-`date +%Y%m%d` to ./package/base-files/files/etc/openwrt_date ..."
-echo "[$(date "+%H:%M:%S")] Writing $TARGET_PROFILE to ./package/base-files/files/etc/openwrt_device ..."
+echo "$TARGET_PROFILE" > ./package/base-files/files/etc/openwrt_device
 }
 
 Diy-Part3() {
 GET_TARGET_INFO
-Default_Firmware=openwrt-$TARGET_BOARD-$TARGET_SUBTARGET-$TARGET_PROFILE-$FIRMWARE_SUFFIX
+Default_Firmware=openwrt-$TARGET_BOARD-$TARGET_SUBTARGET-$TARGET_PROFILE-squashfs-sysupgrade.bin
 AutoBuild_Firmware=AutoBuild-$TARGET_PROFILE-Lede-$Lede_Version`(date +-%Y%m%d.bin)`
 AutoBuild_Detail=AutoBuild-$TARGET_PROFILE-Lede-$Lede_Version`(date +-%Y%m%d.detail)`
 mkdir -p ./bin/Firmware
+echo "[$(date "+%H:%M:%S")] Moving $Default_Firmware to /bin/Firmware/$AutoBuild_Firmware ..."
 mv ./bin/targets/$TARGET_BOARD/$TARGET_SUBTARGET/$Default_Firmware ./bin/Firmware/$AutoBuild_Firmware
-cd ./bin/Firmware
-Firmware_Size=`ls -l $AutoBuild_Firmware | awk '{print $5}'`
-Firmware_Size_MB=`awk 'BEGIN{printf "固件大小:%.2fMB\n",'$((Firmware_Size))'/1000000}'`
-Firmware_MD5=`md5sum $AutoBuild_Firmware | cut -d ' ' -f1`
-Firmware_SHA256=`sha256sum $AutoBuild_Firmware | cut -d ' ' -f1`
-echo "$Firmware_Size_MB" > ./$AutoBuild_Detail
-echo -e "编译日期:$Compile_Time\n" >> ./$AutoBuild_Detail
-echo -e "MD5:$Firmware_MD5\nSHA256:$Firmware_SHA256" >> ./$AutoBuild_Detail
-cd ../..
+echo "[$(date "+%H:%M:%S")] Calculating MD5 and SHA256 ..."
+Firmware_MD5=`md5sum ./bin/Firmware/$AutoBuild_Firmware | cut -d ' ' -f1`
+Firmware_SHA256=`sha256sum ./bin/Firmware/$AutoBuild_Firmware | cut -d ' ' -f1`
+echo "编译日期:$Compile_Time" > ./bin/Firmware/$AutoBuild_Detail
+echo -e "\nMD5:$Firmware_MD5\nSHA256:$Firmware_SHA256" >> ./bin/Firmware/$AutoBuild_Detail
 }
