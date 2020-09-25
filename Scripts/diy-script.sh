6e3d0b637b37d6d03158b2309219411df66b17fb
Hyy2001X <1804430051@qq.com>
2020-09-25 11:30:20 +0800

    Add files via upload

--- Scripts/diy-script.sh
+++ Scripts/diy-script.sh
@@ -47,10 +47,11 @@ done
 }
 
 Diy-Part1() {
-pwd
-mv ../Customize/mac80211.sh ./package/kernel/mac80211/files/lib/wifi/mac80211.sh
-mv ../Customize/AutoUpdate.sh ./package/base-files/files/bin/AutoUpdate.sh
-mv ../Customize/system ./package/base-files/files/etc/config/system
+ls $GITHUB_WORKSPACE
+cd .. && pwd && ls
+mv /workdir/Customize/mac80211.sh ./package/kernel/mac80211/files/lib/wifi/mac80211.sh
+mv /workdir/Customize/AutoUpdate.sh ./package/base-files/files/bin/AutoUpdate.sh
+mv /workdir/Customize/system ./package/base-files/files/etc/config/system
 #mv ../Customize/mwan3 ./package/feeds/packages/mwan3/files/etc/config/mwan3
 sed -i "s/#src-git helloworld/src-git helloworld/g" feeds.conf.default
 [ ! -d ./package/lean ] && mkdir ./package/lean
