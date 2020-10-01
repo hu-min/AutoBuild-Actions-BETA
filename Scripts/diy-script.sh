40fe2b81c03149c2de7396696cde81fddb963226
Hyy2001X <1804430051@qq.com>
2020-10-01 21:11:04 +0800

    Merge code 20201001

--- Scripts/diy-script.sh
+++ Scripts/diy-script.sh
@@ -33,11 +33,16 @@ do
 	fi
 	if [ -f $2/Makefile ] || [ -f $2/README* ];then
 		echo "[$(date "+%H:%M:%S")] Package $2 detected!"
-		if [ $2 == OpenClash ];then
-			mv $2/luci-app-openclash ./package/lean
-		else
-			mv $2 ./package/lean
-		fi
+		case $2 in
+		OpenClash)
+			mv ./$2/luci-app-openclash ./package/lean
+		;;
+		openwrt-OpenAppFilter)
+			mv ./$2 ./package/lean
+		;;
+		*)
+			mv ./$2 ./package/lean
+		esac
 		rm -rf ./$2 > /dev/null 2>&1
 		break
 	else
@@ -74,10 +79,12 @@ mv2 firewall.config package/network/config/firewall/files
 mv2 banner package/base-files/files/etc
 
 ExtraPackages git luci-theme-argon https://github.com/jerrykuku 18.06
-ExtraPackages svn luci-app-adguardhome https://github.com/Lienol/openwrt/trunk/package/diy
+#ExtraPackages svn luci-app-adguardhome https://github.com/Lienol/openwrt/trunk/package/diy
+ExtraPackages git luci-app-adguardhome https://github.com/Hyy2001X master
 ExtraPackages svn luci-app-smartdns https://github.com/project-openwrt/openwrt/trunk/package/ntlf9t
 ExtraPackages svn smartdns https://github.com/project-openwrt/openwrt/trunk/package/ntlf9t
 ExtraPackages git OpenClash https://github.com/vernesong master
+ExtraPackages git openwrt-OpenAppFilter https://github.com/Lienol master
 ExtraPackages git luci-app-serverchan https://github.com/tty228 master
 ExtraPackages svn luci-app-socat https://github.com/xiaorouji/openwrt-package/trunk/lienol
 }
