0f7b064a02cc5dfc13661232bc957df1cdda237c
Hyy2001X <1804430051@qq.com>
2020-11-12 12:05:16 +0800

    Merge code [test]

--- Scripts/diy-script.sh
+++ Scripts/diy-script.sh
@@ -9,53 +9,43 @@ Default_Device=d-team_newifi-d2
 
 AutoUpdate_Version=`awk 'NR==6' ./package/base-files/files/bin/AutoUpdate.sh | awk -F'[="]+' '/Version/{print $2}'`
 Compile_Date=`date +'%Y/%m/%d'`
-echo "Compile_Time="`date +'%Y-%m-%d %H:%M:%S'`"" > $GITHUB_WORKSPACE/Diy_Core.txt
+Compile_Time=`date +'%Y-%m-%d %H:%M:%S'`
 Default_File=./package/lean/default-settings/files/zzz-default-settings
 Lede_Version=`egrep -o "R[0-9]+\.[0-9]+\.[0-9]+" $Default_File`
-echo "Openwrt_Version="$Lede_Version-`date +%Y%m%d`"" >> $GITHUB_WORKSPACE/Diy_Core.txt
+Openwrt_Version="$Lede_Version-`date +%Y%m%d`"
+}
+
+GET_TARGET_INFO() {
+Diy_Core
 TARGET_PROFILE=`egrep -o "CONFIG_TARGET.*DEVICE.*=y" .config | sed -r 's/.*DEVICE_(.*)=y/\1/'`
-if [ -z $TARGET_PROFILE ];then
-	echo "TARGET_PROFILE=$Default_Device" >> $GITHUB_WORKSPACE/Diy_Core.txt
-else
-	echo "TARGET_PROFILE=$TARGET_PROFILE" >> $GITHUB_WORKSPACE/Diy_Core.txt
-fi
-echo "TARGET_BOARD=`awk -F'[="]+' '/TARGET_BOARD/{print $2}' .config`" >> $GITHUB_WORKSPACE/Diy_Core.txt
-echo "TARGET_SUBTARGET=`awk -F'[="]+' '/TARGET_SUBTARGET/{print $2}' .config`" >> $GITHUB_WORKSPACE/Diy_Core.txt
+[ -z $TARGET_PROFILE ] && TARGET_PROFILE=$Default_Device
+TARGET_BOARD=`awk -F'[="]+' '/TARGET_BOARD/{print $2}' .config`
+TARGET_SUBTARGET=`awk -F'[="]+' '/TARGET_SUBTARGET/{print $2}' .config`
 }
 
 ExtraPackages() {
-[ -d ./package/lean/$2 ] && rm -rf ./package/lean/$2
-[ -d ./$2 ] && rm -rf ./$2
+[ -d ./package/$2/$3 ] && rm -rf ./package/$2/$3
+[ -d ./$3 ] && rm -rf ./$3
 Retry_Times=3
-while [ ! -f $2/Makefile ]
+while [ ! -f $3/Makefile ]
 do
-	echo "[$(date "+%H:%M:%S")] Checking out $2 from $3 ..."
+	echo "[$(date "+%H:%M:%S")] Checking out package [$2] from $3 ..."
 	case $1 in
 	git)
-		git clone -b $4 $3/$2 $2 > /dev/null 2>&1
+		git clone -b $5 $4/$3 $3 > /dev/null 2>&1
 	;;
 	svn)
-		svn checkout $3/$2 $2 > /dev/null 2>&1
+		svn checkout $4/$3 $3 > /dev/null 2>&1
 	esac
-	if [ -f $2/Makefile ] || [ -f $2/README* ];then
-		echo "[$(date "+%H:%M:%S")] Package $2 is detected!"
-		case $2 in
-		OpenClash)
-			mv -f ./$2/luci-app-openclash ./package/lean
-		;;
-		openwrt-OpenAppFilter)
-			mv -f ./$2 ./package/lean
-		;;
-		*)
-			mv -f ./$2 ./package/lean
-		esac
-		rm -rf ./$2 > /dev/null 2>&1
+	if [ -f $3/Makefile ] || [ -f $3/README* ];then
+		echo "[$(date "+%H:%M:%S")] Package [$3] is detected!"
+		mv -f $3 ./package/lean
 		break
 	else
-		[ $Retry_Times -lt 1 ] && echo "[$(date "+%H:%M:%S")] Skip check out package $1 ..." && break
-		echo "[$(date "+%H:%M:%S")] [$Retry_Times]Checkout failed,retry in 3s ..."
+		[ $Retry_Times -lt 1 ] && echo "[$(date "+%H:%M:%S")] Skip check out package [$3] ..." && break
+		echo "[$(date "+%H:%M:%S")] [$Retry_Times] Checkout failed,retry in 3s ..."
 		Retry_Times=$(($Retry_Times - 1))
-		rm -rf ./$2 > /dev/null 2>&1
+		rm -rf $3 > /dev/null 2>&1
 		sleep 3
 	fi
 done
@@ -86,44 +76,34 @@ Diy-Part1() {
 [ -f feeds.conf.default ] && sed -i "s/#src-git helloworld/src-git helloworld/g" feeds.conf.default
 [ ! -d ./package/lean ] && mkdir ./package/lean
 
-# mv2 feeds.conf.default
 mv2 mac80211.sh package/kernel/mac80211/files/lib/wifi
 mv2 system package/base-files/files/etc/config
 mv2 AutoUpdate.sh package/base-files/files/bin
 mv2 banner package/base-files/files/etc
-mv2 mt76.mk package/kernel/mt76 Makefile
 
-# ExtraPackages svn mac80211 https://github.com/openwrt/openwrt/trunk/package/kernel
-# rm -rf package/kernel/mac80211 && mv -f package/lean/mac80211 package/kernel
-ExtraPackages svn dnsmasq https://github.com/openwrt/openwrt/trunk/package/network/services
-rm -rf package/network/services/dnsmasq && mv -f package/lean/dnsmasq package/network/services
-ExtraPackages svn hostapd https://github.com/openwrt/openwrt/trunk/package/network/services
-rm -rf package/network/services/hostapd && mv -f package/lean/hostapd package/network/services
-ExtraPackages svn dropbear https://github.com/openwrt/openwrt/trunk/package/network/services
-rm -rf package/network/services/dropbear && mv -f package/lean/dropbear package/network/services
+ExtraPackages svn network/services dnsmasq https://github.com/openwrt/openwrt/trunk/package/network/services
+ExtraPackages svn network/services hostapd https://github.com/openwrt/openwrt/trunk/package/network/services
+ExtraPackages svn network/services dropbear https://github.com/openwrt/openwrt/trunk/package/network/services
+ExtraPackages git kernel mt76 https://github.com/openwrt master
 
-ExtraPackages git luci-app-autoupdate https://github.com/Hyy2001X main
-ExtraPackages git luci-theme-argon https://github.com/jerrykuku 18.06
-ExtraPackages git luci-app-argon-config https://github.com/jerrykuku master
-ExtraPackages git luci-app-adguardhome https://github.com/Hyy2001X master
-ExtraPackages svn luci-app-smartdns https://github.com/project-openwrt/openwrt/trunk/package/ntlf9t
-ExtraPackages svn smartdns https://github.com/project-openwrt/openwrt/trunk/package/ntlf9t
-ExtraPackages git OpenClash https://github.com/vernesong master
-ExtraPackages git luci-app-serverchan https://github.com/tty228 master
-ExtraPackages svn luci-app-socat https://github.com/xiaorouji/openwrt-package/trunk/lienol
-# ExtraPackages git openwrt-upx https://github.com/Hyy2001X master
-# ExtraPackages svn luci-app-mentohust https://github.com/project-openwrt/openwrt/trunk/package/ctcgfw
-# ExtraPackages svn mentohust https://github.com/project-openwrt/openwrt/trunk/package/ctcgfw
-#ExtraPackages svn luci-theme-opentomato https://github.com/kenzok8/openwrt-packages/trunk
-#ExtraPackages svn luci-theme-opentomcat https://github.com/kenzok8/openwrt-packages/trunk
-# ExtraPackages svn luci-app-adguardhome https://github.com/Lienol/openwrt/trunk/package/diy
-# ExtraPackages git luci-app-adguardhome https://github.com/rufengsuixing master
-# ExtraPackages git openwrt-OpenAppFilter https://github.com/Lienol master
+ExtraPackages git lean luci-app-autoupdate https://github.com/Hyy2001X main
+ExtraPackages git lean luci-theme-argon https://github.com/jerrykuku 18.06
+ExtraPackages git lean luci-app-argon-config https://github.com/jerrykuku master
+ExtraPackages git lean luci-app-adguardhome https://github.com/Hyy2001X master
+ExtraPackages svn lean luci-app-smartdns https://github.com/project-openwrt/openwrt/trunk/package/ntlf9t
+ExtraPackages svn lean smartdns https://github.com/project-openwrt/openwrt/trunk/package/ntlf9t
+ExtraPackages git lean OpenClash https://github.com/vernesong master
+ExtraPackages git lean luci-app-serverchan https://github.com/tty228 master
+ExtraPackages svn lean luci-app-socat https://github.com/xiaorouji/openwrt-package/trunk/lienol
+# ExtraPackages git lean openwrt-upx https://github.com/Hyy2001X master
+# ExtraPackages svn lean luci-app-mentohust https://github.com/project-openwrt/openwrt/trunk/package/ctcgfw
+# ExtraPackages svn lean mentohust https://github.com/project-openwrt/openwrt/trunk/package/ctcgfw
+# ExtraPackages git lean openwrt-OpenAppFilter https://github.com/Lienol master
+# ExtraPackages svn lean AdGuardHome https://github.com/project-openwrt/openwrt/trunk/package/ntlf9t
 }
 
 Diy-Part2() {
-Diy_Core
-. $GITHUB_WORKSPACE/Diy_Core.txt
+GET_TARGET_INFO
 mv2 mwan3 package/feeds/packages/mwan3/files/etc/config
 echo "Author: $Author"
 echo "Lede Version: $Openwrt_Version"
@@ -135,7 +115,7 @@ sed -i "s?Openwrt?Openwrt $Openwrt_Version / AutoUpdate $AutoUpdate_Version?g" .
 }
 
 Diy-Part3() {
-. $GITHUB_WORKSPACE/Diy_Core.txt
+GET_TARGET_INFO
 Default_Firmware=openwrt-$TARGET_BOARD-$TARGET_SUBTARGET-$TARGET_PROFILE-squashfs-sysupgrade.bin
 AutoBuild_Firmware=AutoBuild-$TARGET_PROFILE-Lede-${Openwrt_Version}.bin
 AutoBuild_Detail=AutoBuild-$TARGET_PROFILE-Lede-${Openwrt_Version}.detail
