5c31c0bccd0102963a42a9d44967d9f1789743dd
Hyy2001X <1804430051@qq.com>
2020-11-13 21:35:55 +0800

    Update diy-script.sh

--- Scripts/diy-script.sh
+++ Scripts/diy-script.sh
@@ -44,7 +44,7 @@ do
 	esac
 	if [ -f $PKG_NAME/Makefile ] || [ -f $PKG_NAME/README* ];then
 		echo "[$(date "+%H:%M:%S")] Package [$PKG_NAME] is detected!"
-		mv -f $PKG_NAME package/$PKG_DIR
+		mv $PKG_NAME package/$PKG_DIR
 		break
 	else
 		[ $Retry_Times -lt 1 ] && echo "[$(date "+%H:%M:%S")] Skip check out package [$PKG_NAME] ..." && break
@@ -79,19 +79,19 @@ fi
 
 Diy-Part1() {
 [ -f feeds.conf.default ] && sed -i "s/#src-git helloworld/src-git helloworld/g" feeds.conf.default
-[ ! -d ./package/lean ] && mkdir ./package/lean
+[ ! -d package/lean ] && mkdir package/lean
 
 mv2 mac80211.sh package/kernel/mac80211/files/lib/wifi
 mv2 system package/base-files/files/etc/config
 mv2 AutoUpdate.sh package/base-files/files/bin
 mv2 banner package/base-files/files/etc
 
-ExtraPackages svn network/services dnsmasq https://github.com/openwrt/openwrt/trunk/package/network/services
+# ExtraPackages svn network/services dnsmasq https://github.com/openwrt/openwrt/trunk/package/network/services
 # ExtraPackages svn network/services hostapd https://github.com/openwrt/openwrt/trunk/package/network/services
-ExtraPackages svn network/services dropbear https://github.com/openwrt/openwrt/trunk/package/network/services
-ExtraPackages svn network/services ppp https://github.com/openwrt/openwrt/trunk/package/network/services
-ExtraPackages git kernel mt76 https://github.com/openwrt master
-ExtraPackages svn firmware linux-firmware https://github.com/openwrt/openwrt/trunk/package/firmware
+# ExtraPackages svn network/services dropbear https://github.com/openwrt/openwrt/trunk/package/network/services
+# ExtraPackages svn network/services ppp https://github.com/openwrt/openwrt/trunk/package/network/services
+# ExtraPackages git kernel mt76 https://github.com/openwrt master
+# ExtraPackages svn firmware linux-firmware https://github.com/openwrt/openwrt/trunk/package/firmware
 
 ExtraPackages git lean luci-app-autoupdate https://github.com/Hyy2001X main
 ExtraPackages git lean luci-theme-argon https://github.com/jerrykuku 18.06
@@ -117,8 +117,8 @@ echo "Lede Version: $Openwrt_Version"
 echo "AutoUpdate Version: $AutoUpdate_Version"
 echo "Router: $TARGET_PROFILE"
 sed -i "s?$Lede_Version?$Lede_Version Compiled by $Author [$Compile_Date]?g" $Default_File
-echo "$Openwrt_Version" > ./package/base-files/files/etc/openwrt_info
-sed -i "s?Openwrt?Openwrt $Openwrt_Version / AutoUpdate $AutoUpdate_Version?g" ./package/base-files/files/etc/banner
+echo "$Openwrt_Version" > package/base-files/files/etc/openwrt_info
+sed -i "s?Openwrt?Openwrt $Openwrt_Version / AutoUpdate $AutoUpdate_Version?g" package/base-files/files/etc/banner
 }
 
 Diy-Part3() {
@@ -126,13 +126,13 @@ GET_TARGET_INFO
 Default_Firmware=openwrt-$TARGET_BOARD-$TARGET_SUBTARGET-$TARGET_PROFILE-squashfs-sysupgrade.bin
 AutoBuild_Firmware=AutoBuild-$TARGET_PROFILE-Lede-${Openwrt_Version}.bin
 AutoBuild_Detail=AutoBuild-$TARGET_PROFILE-Lede-${Openwrt_Version}.detail
-mkdir -p ./bin/Firmware
+mkdir -p bin/Firmware
 echo "Firmware: $AutoBuild_Firmware"
-mv ./bin/targets/$TARGET_BOARD/$TARGET_SUBTARGET/$Default_Firmware ./bin/Firmware/$AutoBuild_Firmware
+mv bin/targets/$TARGET_BOARD/$TARGET_SUBTARGET/$Default_Firmware bin/Firmware/$AutoBuild_Firmware
 echo "[$(date "+%H:%M:%S")] Calculating MD5 and SHA256 ..."
-Firmware_MD5=$(md5sum ./bin/Firmware/$AutoBuild_Firmware | cut -d ' ' -f1)
-Firmware_SHA256=$(sha256sum ./bin/Firmware/$AutoBuild_Firmware | cut -d ' ' -f1)
+Firmware_MD5=$(md5sum bin/Firmware/$AutoBuild_Firmware | cut -d ' ' -f1)
+Firmware_SHA256=$(sha256sum bin/Firmware/$AutoBuild_Firmware | cut -d ' ' -f1)
 echo -e "MD5: $Firmware_MD5\nSHA256: $Firmware_SHA256"
-echo "编译日期:$Compile_Time" > ./bin/Firmware/$AutoBuild_Detail
-echo -e "\nMD5:$Firmware_MD5\nSHA256:$Firmware_SHA256" >> ./bin/Firmware/$AutoBuild_Detail
+echo "编译日期:$Compile_Time" > bin/Firmware/$AutoBuild_Detail
+echo -e "\nMD5:$Firmware_MD5\nSHA256:$Firmware_SHA256" >> bin/Firmware/$AutoBuild_Detail
 }
